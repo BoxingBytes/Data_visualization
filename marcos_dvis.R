cd15753ad3c4168b366309e1f37c8a75cc576328
Marcos Duchinski <92726479+marcosduchinski@users.noreply.github.com>
2025-12-10 16:31:11 +0100

    reading data from parquet file

--- marcos_dvis.R
+++ marcos_dvis.R
@@ -3,75 +3,9 @@ library(dplyr)
 library(ggplot2)
 library(lubridate )
 library(sf)
+library(arrrow)
 
-col_spec <- cols(.default = "c")
-
-val_2025_first <- read_csv2("data/validations-reseau-ferre-nombre-validations-par-jour-1er-trimestre.csv", col_types = col_spec)
-val_2025_second <- read_csv2("data/validations-reseau-ferre-nombre-validations-par-jour-2eme-trimestre.csv", col_types = col_spec)
-val_2018_first <- read_delim("data/data-rf-2018/2018_S1_NB_FER.txt", delim = "\t", col_types = col_spec)
-val_2018_second <- read_delim("data/data-rf-2018/2018_S2_NB_FER.txt", delim = "\t",col_types = col_spec)
-val_2019_first <- read_delim("data/data-rf-2019/2019_S1_NB_FER.txt", delim = "\t",col_types = col_spec)
-val_2019_second <- read_delim("data/data-rf-2019/2019_S2_NB_FER.txt", delim = "\t",col_types = col_spec)
-val_2020_first <- read_delim("data/data-rf-2020/2020_S1_NB_FER.txt", delim = "\t",col_types = col_spec)
-val_2020_second <- read_delim("data/data-rf-2020/2020_S2_NB_FER.txt", delim = "\t",col_types = col_spec)
-val_2021_first <- read_delim("data/data-rf-2021/2021_S1_NB_FER.txt", delim = "\t", col_types = col_spec)
-val_2021_second <- read_delim("data/data-rf-2021/2021_S2_NB_FER.txt", delim = "\t", col_types = col_spec)
-val_2022_first <- read_delim("data/data-rf-2022/2022_S1_NB_FER.txt", delim = "\t", col_types = col_spec)
-val_2022_second <- read_delim("data/data-rf-2022/2022_S2_NB_FER.txt", delim = ";", col_types = col_spec)
-val_2023_first <- read_delim("data/data-rf-2023/2023_S1_NB_FER.txt", delim = "\t", col_types = col_spec)
-val_2023_second <- read_delim("data/data-rf-2023/2023_S2_NB_FER.txt", delim = "\t", locale = locale(encoding = "UTF-16"), col_types = col_spec)
-val_2024_first <- read_delim("data/data-rf-2024/2024_S1_NB_FER.txt", delim = "\t", col_types = col_spec)
-
-val_2025_second <- rename(val_2025_second, ID_REFA_LDA = ID_ZDC)
-val_2025_first <- rename(val_2025_first, ID_REFA_LDA = ID_ZDC)
-
-
-########## Convert date here would be nice  d-m-y
-
-
-val_2023_second <- rename(val_2023_second, ID_REFA_LDA = ID_ZDC)
-val_2023_first <- rename(val_2023_first, ID_REFA_LDA = lda)
-val_2024_first <- rename(val_2024_first, ID_REFA_LDA = ID_ZDC)
-val_2022_second <- rename(val_2022_second, ID_REFA_LDA = lda)
-
-
-
-zd <- st_read("data/REF_ZdA/PL_ZDL_R_11-11-2025.shp" )
-
-
-val_merged <- bind_rows(
-  val_2018_first,
-  val_2018_second,
-  val_2019_first,
-  val_2019_second,
-  val_2020_first,
-  val_2020_second,
-  val_2021_first,
-  val_2021_second,
-  val_2022_first,
-  val_2022_second,
-  val_2023_first,
-  val_2023_second,
-  val_2024_first,
-  val_2025_first,
-  val_2025_second
-)
-
-val_merged <- val_merged %>%
-  mutate(JOUR = parse_date_time(JOUR, orders = c("ymd", "mdy", "dmy")))
-val_merged$NB_VALD <- as.numeric(val_merged$NB_VALD)
-
-zd <- zd |>
-  filter(type_arret != "Arrêt de bus")
-
-zd <- zd |> mutate(across(-geometry, as.character))
-
-zd_grouped <- zd %>%
-  group_by(idrefa_lda) %>%
-  summarise(geometry = st_union(geometry))
-
-val_zd <- left_join(val_merged, zd_grouped, by = c("ID_REFA_LDA" = "idrefa_lda"))
-str(val_zd)
+val_merged <- read_parquet("validations.parquet")
 
 # group per year and season
 df_season <- val_merged %>%
@@ -153,7 +87,7 @@ ggplot(df_month, aes(x = year_month, y = NB_VALD)) +
                 ymin = -Inf, ymax = Inf),
             inherit.aes = FALSE, alpha = 0.2) +
   geom_rect(aes( xmin = olymp_start, xmax = olymp_end,
-                ymin = -Inf, ymax = Inf),
+                 ymin = -Inf, ymax = Inf),
             inherit.aes = FALSE, alpha = 0.2) +
   # validations area
   geom_area( fill  ="#3498DB",  alpha = 0.5) +
@@ -167,7 +101,4 @@ ggplot(df_month, aes(x = year_month, y = NB_VALD)) +
     subtitle = "Shadows = COVID, Olimpic Games; dashed = mean"
   ) +
   theme_classic()
-
-
-
 
